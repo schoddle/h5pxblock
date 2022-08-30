5bae9fc3fa27722ec2c7fa655f2a9c2e05c6e0d7
zia.fazal@arbisoft.com <zia.fazal@arbisoft.com>
2022-08-30 15:56:08 +0500

    feat: cypress e2e tests
    feat: added package-lock file
    
    chore: version bump

---
+++ tests/cypress/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    env: {
+      STUDIO_URL: "http://localhost:8000/scenario/h5pxblock.0/studio_view/",
+      LMS_URL: "http://localhost:8000/scenario/h5pxblock.0/"
+    }
+  },
+});
